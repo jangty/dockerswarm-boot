44c11087b6be54a5498550317693aee43214d92b
jang.taeyoung <jang.taeyoung@joins.com>
2020-06-15 17:05:20 +0900

    환경변수 사용

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-source .env
+source ./.env
 
+sed "s/%APPLICATION_NAME%/${APPLICATION_NAME}/g" -i entrypoint.sh
+sed "s/%APPLICATION_NAME%/${APPLICATION_NAME}/g" -i Dockerfile
+sed "s/%APPLICATION_NAME%/${APPLICATION_NAME}/g" -i docker-stack.yml
+
+sed "s/%HTTP_PORT%/${HTTP_PORT}/g" -i docker-stack.yml
+sed "s/%AJP_PORT%/${AJP_PORT}/g" -i docker-stack.yml
+sed "s/%ADMIN_PORT%/${ADMIN_PORT}/g" -i docker-stack.yml
+
+
+## image build
 docker build -t ${REGISTRY}/${APPLICATION_NAME}  .
 docker tag ${REGISTRY}/${APPLICATION_NAME}:latest ${REGISTRY}/${APPLICATION_NAME}:latest
 docker push ${REGISTRY}/${APPLICATION_NAME}
 
-sed "s/%APPLICATION_NAME%/${APPLICATION_NAME}/g" -i scripts/entrypoint.sh
+
+
