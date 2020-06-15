50f8c75330bdac296271d7fa13047fcb557ecc01
jang.taeyoung <jang.taeyoung@joins.com>
2020-06-15 15:28:40 +0900

    scouter.conf 경로 수정

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -18,12 +18,12 @@ net_collector_tcp_port=6040
 
 #hook_method_patterns=${HOOK_METHOD_PATTERNS:-org.mybatis.jpetstore.*.*}
 
-sed "s/%OBJ_NAME%/${obj_name}/g" -i scouter/conf/scouter.conf
-sed "s/%OBJ_HOST_NAME%/${obj_host_name}/g" -i scouter/conf/scouter.conf
-sed "s/%NET_COLLECTOR_IP%/${net_collector_ip}/g" -i scouter/conf/scouter.conf
-sed "s/%NET_COLLECTOR_UDP_PORT%/${net_collector_udp_port}/g" -i scouter/conf/scouter.conf
-sed "s/%NET_COLLECTOR_TCP_PORT%/${net_collector_tcp_port}/g" -i scouter/conf/scouter.conf
-#sed "s/%HOOK_METHOD_PATTERNS%/${hook_method_patterns}/g" -i scouter/conf/scouter.conf
+sed "s/%OBJ_NAME%/${obj_name}/g" -i scouter/scouter.conf
+sed "s/%OBJ_HOST_NAME%/${obj_host_name}/g" -i scouter/scouter.conf
+sed "s/%NET_COLLECTOR_IP%/${net_collector_ip}/g" -i scouter/scouter.conf
+sed "s/%NET_COLLECTOR_UDP_PORT%/${net_collector_udp_port}/g" -i scouter/scouter.conf
+sed "s/%NET_COLLECTOR_TCP_PORT%/${net_collector_tcp_port}/g" -i scouter/scouter.conf
+#sed "s/%HOOK_METHOD_PATTERNS%/${hook_method_patterns}/g" -i scouter/scouter.conf
 
 
 java -Dspring.profiles.active=dev -Dmaven.test.skip=true -Xms128m -Xmx1024m -javaagent:scouter/scouter.agent.jar -Dscouter.config=scouter/scouter.conf -jar app.jar
