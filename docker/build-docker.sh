d47cd6b2078e6dbd7e547fd5cefbb9a49a076f58
joins_gitlab <admin@example.com>
2020-07-23 15:29:16 +0900

    Update build-docker.sh

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -8,7 +8,7 @@ docker build -t ${REGISTRY}/${APPLICATION_NAME} -f docker/Dockerfile .
 docker tag ${REGISTRY}/${APPLICATION_NAME}:latest ${REGISTRY}/${APPLICATION_NAME}:latest
 
 # login to image registry
-docker login -u jenkins -p jenkins ${REGISTRY}
+docker login -u jenkins -p jenkins123!@# ${REGISTRY}
 
 # image push
 docker push ${REGISTRY}/${APPLICATION_NAME}
